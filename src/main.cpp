10bccce2eb43ffa5ce89736e108b22e11741e6fc
Jukeland <antonisx113@gmail.com>
2023-12-09 06:20:15 +0200

    xd

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,72 @@
-#include <iostream>
+//  #######################################################################################
+//                                   Platform Globals
+//  #######################################################################################
 
 static bool running = true;
 
+//  #######################################################################################
+//                                  Platform Functions
+//  #######################################################################################
+
+bool platformCreateWindow(int width, int height, char* title);
+
+//  #######################################################################################
+//                                  Windows Platform
+//  #######################################################################################
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+
+//  #######################################################################################
+//                                  Windows Globals
+//  #######################################################################################
+
+
+
+//  #######################################################################################
+//                                Platform Implementations
+//  #######################################################################################
+
+bool platformCreateWindow(int width, int height, char* title){
+
+    HINSTANCE instance = GetModuleHandleA(0);
+
+    WNDCLASSA wc = {};
+    wc.hInstance = instance;
+    wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = title;           // Not the title
+    wc.lpfnWndProc = DefWindowProcA;    // Callbakck for input into the window
+
+    if(!RegisterClassA(&wc)){
+        return false;
+    }
+
+    int dwStyle = WS_OVERLAPPEDWINDOW;
+
+    HWND window = CreateWindowExA(0, title, title, dwStyle, 100, 100, width, height, 
+                                    NULL, NULL, instance, NULL);
+
+    if(window == NULL)
+        return false;
+
+    ShowWindow(window, SW_SHOW);
+
+    return true;
+
+}
+
+
+#endif
+
 int main(){
 
-    int n = 0;
-    std::cout << "XD? " << n << std::endl;
-
+    platformCreateWindow(1200, 720, "Dog");
     while(running){
 
         // Update
-        n++;
-        if(n < 10)
-            std:: cout << "XD?\n";
 
     }
 
